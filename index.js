d7f26ede57944d702cfabfa6228ef4792fd31c9e
Kalamon9 <77126864+Kalamon9@users.noreply.github.com>
2021-07-26 21:11:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-//alert('k');
+const hamburger = document.querySelector('.hamburger')
+    const navLinks = document.querySelector('.nav-links')
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active')
+      });
+
+
+
 
 
 function footer() {
